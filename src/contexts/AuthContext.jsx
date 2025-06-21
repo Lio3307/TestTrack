4cefb9896e3535f323b4d417fd2155c1d3737b5e
Lio3307 <aureliofs01@gmail.com>
2025-06-21 12:26:31 +0800

    create signUpEmail function

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,12 +1,52 @@
 import { useState, createContext, useContext } from "react";
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { addDoc, doc } from "firebase/firestore";
 
 const AuthProvider = createContext()
 
 export const AuthContext = ({children}) => {
 
-    function signInEmail() {
-        
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [username, setUsername] = useState('')
+
+    const [loading, setLoading] = useState(true)
+
+    async function signInEmail(auth, email, password) {
+        if(!email.trim() || !password.trim()) {
+            
+        }
+
+        try {
+            
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    async function signUpEmail (auth, db, username, email, password) {
+        if(!email.trim() || !password.trim() || !username.trim()) {
+            alert ("Input Field Cannot Empty!!")
+            return;
+        }
+        if(password.length < 6 && typeof password ===  'string') {
+            alert("Create Password At Least 6 Character!!")
+        }
+        try {
+            setLoading(true)
+            await createUserWithEmailAndPassword(auth, email, password)
+            const user = auth.currentUser
+            const docRef = doc(db, "Users", user.uid)
+            await addDoc(docRef, {
+                userId: user.uid,
+                username: username,
+                email: email,
+            })
+        } catch (err) {
+            console.error(err)
+        } finally {
+            setLoading(false)
+        }
     }
 
     return (
