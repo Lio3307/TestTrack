4269574fa937d3c4d6233419c3f6437fe42417af
Lio3307 <aureliofs01@gmail.com>
2025-06-21 17:02:59 +0800

    cerating add activity component

--- src/Components/AddAct.jsx
+++ src/Components/AddAct.jsx
@@ -1,36 +1,68 @@
-import { collection, getDocs, serverTimestamp } from "firebase/firestore"
-import { auth, db } from "../firebase/config"
-import { useAuthContext } from "../contexts/AuthContext"
-
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { auth, db } from "../firebase/config";
+import { useState } from "react";
 
 export const AddAct = () => {
+  const [titleAct, setTitleAct] = useState("");
+  const [textAct, setTextAct] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const {loading, setLoading} = useAuthContext()
-    const []
-
-    const addActivityHanlder = async () => {
-        try {
-            setLoading(true)
-            const user = auth.currentUser
-            const subCollection = collection(db, "Users", user.uid, "Activity")
-            await addDoc(subCollection, {
-                titleActivity: ,
-                textActivity: ,
-                userId: user.uid,
-                createdAt: serverTimestamp()
-
-            })
-        } catch (err) {
-            console.error(err)
-        } finally {
-            setLoading(false)
-        }
+  const addActivityHanlder = async () => {
+    try {
+      setLoading(true);
+      const user = auth.currentUser;
+      const subCollection = collection(db, "Users", user.uid, "Activity");
+      await addDoc(subCollection, {
+        titleActivity: titleAct,
+        textActivity: textAct,
+        userId: user.uid,
+        createdAt: serverTimestamp(),
+      });
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+      alert("Activity Has Been Created!!");
     }
+  };
 
-    return (
+  return (
+    <>
+      {loading ? (
         <>
-            <label>Create Title</label>
-            <input type="text" />
+          <h3>Creating Activity...</h3>
         </>
-    )
-}
+      ) : (
+        <>
+          <label>Create Title</label>
+          <input
+            value={titleAct}
+            onChange={(e) => {
+              setTitleAct(e.target.value);
+            }}
+            type="text"
+          />
+
+          <label>Create Text</label>
+          <textarea
+            value={textAct}
+            onChange={(e) => {
+              setTextAct(e.target.value);
+            }}
+            type="text"
+          />
+
+          <button
+            disabled={loading}
+            onClick={(e) => {
+              e.preventDefault();
+              addActivityHanlder();
+            }}
+          >
+            Create Activity
+          </button>
+        </>
+      )}
+    </>
+  );
+};
