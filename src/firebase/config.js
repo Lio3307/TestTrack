6401959da93c712cbb9ef18e06a844b9ee5b0efe
Lio3307 <aureliofs01@gmail.com>
2025-07-16 17:21:56 +0800

    change .env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,19 +4,18 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZbDdn2XV8vIE3vr8Z-tN0RqZD8LIm00A",
-  authDomain: "trackact-962ac.firebaseapp.com",
-  projectId: "trackact-962ac",
-  storageBucket: "trackact-962ac.firebasestorage.app",
-  messagingSenderId: "1041130770179",
-  appId: "1:1041130770179:web:ef7a97611d45f5acf62336",
-  measurementId: "G-39W9GZ00ET"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MASSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIRBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-const auth = getAuth(app)
-const db = getFirestore(app)
-const googleProvider = new GoogleAuthProvider()
+const auth = getAuth(app);
+const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
 
-export {auth, db, googleProvider}
+export { auth, db, googleProvider };
